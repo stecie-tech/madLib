6f93dc15071546daf558feb6109c449081c3f42c
stecie-tech <76773278+stecie-tech@users.noreply.github.com>
2021-01-27 19:58:07 +0200

    Create madLib.py

---
+++ madLib.py
@@ -0,0 +1,9 @@
+
+name = str(input("Enter the ur name : "))
+age = str(input("Enter ur age : "))
+school = str(input("Enter ur school :"))
+hobby = str(input("what's ur hobby? "))
+leisure = str(input("What do u do in ur leisure tym?"))
+
+print("Hey "+name+" u are "+age+" u study "+school+" ur hobby is "+hobby+" and u "+leisure+" in ur leisure time. thx for ur tym")
+
